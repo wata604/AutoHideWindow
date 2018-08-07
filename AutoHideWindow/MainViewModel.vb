4d70db119e3c1993d94030cfad1a2cfbb2701e6d
Takehiro Watanabe <takehiro78@gmail.com>
2018-08-07 21:50:33 +0900

    feat: 高 DPI 対応

--- AutoHideWindow/MainViewModel.vb
+++ AutoHideWindow/MainViewModel.vb
@@ -1,5 +1,7 @@
 ﻿Imports System.ComponentModel
+Imports System.Runtime.InteropServices
 Imports System.Windows.Forms
+Imports System.Windows.Interop
 
 Public Class MainViewModel
     Implements INotifyPropertyChanged
@@ -95,6 +97,10 @@ Public Class MainViewModel
 
     Public Sub New()
 
+        Dim mainWindow As Window = Nothing
+        Dim dpiScaleFactor As Point = New Point(1.0, 1.0)
+        Dim handle As IntPtr = Nothing
+
         Dim timer As Threading.DispatcherTimer = New Threading.DispatcherTimer With {
             .Interval = TimeSpan.FromSeconds(0.1)
         }
@@ -102,10 +108,32 @@ Public Class MainViewModel
             Sub(sender As Object, e As EventArgs)
                 Me.MouseX = Cursor.Position.X
                 Me.MouseY = Cursor.Position.Y
-                If Me.MouseX >= Me.WindowLeft AndAlso
-                    Me.MouseX <= Me.WindowLeft + Me.WindowWidth AndAlso
-                    Me.MouseY >= Me.WindowTop AndAlso
-                    Me.MouseY <= Me.WindowTop + Me.WindowHeight Then
+
+                If mainWindow Is Nothing Then
+                    Dim x = HwndSource.FromHwnd(Process.GetCurrentProcess().MainWindowHandle)
+                    mainWindow = CType(x.RootVisual, Window)
+                    dpiScaleFactor = mainWindow.GetDpiScaleFactor()
+                    handle = New WindowInteropHelper(mainWindow).Handle
+                End If
+
+                Dim deviceLeft = Me.WindowLeft * dpiScaleFactor.X
+                Dim deviceTop = Me.WindowTop * dpiScaleFactor.Y
+                Dim deviceWidth = Me.WindowWidth * dpiScaleFactor.X
+                Dim deviceHeight = Me.WindowHeight * dpiScaleFactor.Y
+
+                SetWindowPos(
+                    handle,
+                    HWND_TOP,
+                    CType(Math.Round(deviceLeft), Integer),
+                    CType(Math.Round(deviceTop), Integer),
+                    CType(Math.Round(deviceWidth), Integer),
+                    CType(Math.Round(deviceHeight), Integer),
+                    SWP_NOSIZE)
+
+                If Me.MouseX >= deviceLeft AndAlso
+                    Me.MouseX <= deviceLeft + deviceWidth AndAlso
+                    Me.MouseY >= deviceTop AndAlso
+                    Me.MouseY <= deviceTop + deviceHeight Then
                     Me.IsContains = True
                 Else
                     Me.IsContains = False
@@ -129,5 +157,34 @@ Public Class MainViewModel
             End Sub
         timer.Start()
     End Sub
+
+    <DllImport("user32.dll", CharSet:=CharSet.Auto)>
+    Private Shared Function SetWindowPos(
+                            ByVal hWnd As IntPtr,
+                            ByVal hWndInsertAfter As Integer,
+                            ByVal x As Integer,
+                            ByVal y As Integer,
+                            ByVal cx As Integer,
+                            ByVal cy As Integer,
+                            ByVal uFlags As Integer) _
+                            As Boolean
+    End Function
+
+    ' 最前面
+    Private Const HWND_TOPMOST As Integer = -1
+    ' 前面
+    Private Const HWND_TOP As Integer = 0
+    ' すべての最前面ウィンドウの後ろ
+    Private Const HWND_NOTOPMOST As Integer = -2
+    ' Z オーダーの最後
+    Private Const HWND_BOTTOM As Integer = 1
+
+    ''ウィンドウのサイズと位置の変更に関するフラグ
+    ' ウィンドウを表示
+    Private Const SWP_SHOWWINDOW As Integer = &H40
+    ' 現在のサイズを維持（cx、cyは無視）
+    Private Const SWP_NOSIZE As Integer = &H1
+    ' 現在の位置を維持します（x、yは無視）
+    Private Const SWP_NOMOVE As Integer = &H2
 
 End Class
---
+++ AutoHideWindow/ViewExtensions.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Runtime.CompilerServices
+
+Module ViewExtensions
+    <Extension()>
+    Public Function GetDpiScaleFactor(visual As Visual) As Point
+        Dim source = PresentationSource.FromVisual(visual)
+        If Not source Is Nothing AndAlso Not source.CompositionTarget Is Nothing Then
+            Return New Point(
+                source.CompositionTarget.TransformToDevice.M11,
+                source.CompositionTarget.TransformToDevice.M22)
+        End If
+        Return New Point(1.0, 1.0)
+    End Function
+
+End Module
